2990eef7c77bae91a033e0a14ba08f17497f7819
Nelson.Huang <xinchin@gmail.com>
2020-08-04 13:50:11 +0800

    AsyncCallback

---
+++ AsyncCallbackDemo/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace AsyncCallbackDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(24, 26);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(474, 22);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "D:\\RD_2020\\Task\\test\\001.txt";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(615, 24);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(24, 66);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(85, 16);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "radioButton1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(137, 66);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(85, 16);
+            this.radioButton2.TabIndex = 3;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "radioButton2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(24, 111);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(474, 278);
+            this.textBox2.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ AsyncCallbackDemo/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.IO;
+
+namespace AsyncCallbackDemo
+{
+    public partial class Form1 : Form
+    {
+
+        private FileStream myFileStream = default(FileStream);
+        private Boolean blnProcess = default(Boolean);
+        private string nl = Environment.NewLine;
+        private byte[] bteData = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ReadBulkData()
+        {
+            long intI = 0;
+            string strFilePathe = null;
+            try
+            {
+                strFilePathe = textBox1.Text;
+                myFileStream = new FileStream(strFilePathe, FileMode.Open, FileAccess.ReadWrite, FileShare.ReadWrite, 1024, true);
+                AsyncCallback myAC = new AsyncCallback(AsyncReadCallBack);
+                blnProcess = true;
+                myFileStream.BeginRead(bteData, 0, 1024, myAC, null);
+                ProcessMessage("R");
+                myFileStream.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void ProcessMessage(string strRW)
+        {
+            string strMessage = "";
+            if (strRW == "R")
+            {
+                strMessage = "Read";
+            }
+            else
+            {
+                strMessage = "Write";
+            }
+            while (blnProcess == true)
+            {
+                textBox2.Text = textBox2.Text + "---" + strMessage + "----" + nl;
+            }
+            Thread.Sleep(1000);
+            textBox2.Text = textBox2.Text + "------" + blnProcess.ToString() + "------" + nl;
+
+        }
+
+        private void AsyncReadCallBack(IAsyncResult myIar)
+        {
+            long lngRead = 0;
+            Thread.Sleep(1000);
+            blnProcess = false;
+            lngRead = myFileStream.EndRead(myIar);
+            textBox2.Text = textBox2.Text + "----" + lngRead.ToString() + "-------" + nl;
+        }
+
+        private void AsyncWriteCallBack(IAsyncResult myIar)
+        {
+            Thread.Sleep(1000);
+            blnProcess = false;
+            myFileStream.EndWrite(myIar);
+            textBox2.Text = textBox2.Text + "---- Writed -------" + nl;
+        }
+
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Thread myThread = default(Thread);
+            TextBox.CheckForIllegalCrossThreadCalls = false;
+            textBox2.Text = "";
+            try
+            {
+                if (true == radioButton1.Checked)
+                {
+                    myThread = new Thread(WriteBulkData);
+                    myThread.Start();
+
+                }
+                else if (true == radioButton2.Checked)
+                {
+                    myThread = new Thread(ReadBulkData);
+                    myThread.Start();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
+
+        private void WriteBulkData()
+        {
+            long intI = 0;
+            string strFilePath = null;
+            try
+            {
+                strFilePath = textBox1.Text;
+                myFileStream = new FileStream(strFilePath, FileMode.Open, FileAccess.ReadWrite, FileShare.ReadWrite, 1024, true);
+                AsyncCallback myAC = new AsyncCallback(AsyncWriteCallBack);
+                blnProcess = true;
+                bteData = new byte[int.Parse(myFileStream.Length.ToString())];
+                myFileStream.BeginWrite(bteData, 0, 1024, myAC, null);
+                ProcessMessage("W");
+                myFileStream.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
